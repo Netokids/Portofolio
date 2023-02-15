9fa14dfe6b0fbd7ec58a1baf3a37bb1985c951a0
Dion Novalino <51320046+Netokids@users.noreply.github.com>
2023-02-15 14:32:47 +0700

    Update skill.js

--- src/pages/Skill/skill.js
+++ src/pages/Skill/skill.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+import { Col, Row } from "react-bootstrap";
 
 function Skill() {
   return (
     <>
+
       <div style={{
         display: 'flex',
         justifyContent: 'center',
-        
         margin: '20px',
       }}>
         <h1>Skills</h1>
@@ -16,95 +17,102 @@ function Skill() {
       <div style={{
         display: 'flex',
         justifyContent: 'center',
-        flexDirection: 'row',
+
       }}>
-        {[
-          'Dark',
-        ].map((variant) => (
-          <Card
-            bg={variant.toLowerCase()}
-            key={variant}
-            text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
-            style={{ width: '24rem', margin: '20px' }}
-            className="mb-2"
-          >
-            <Card.Header>Frontend</Card.Header>
-            <Card.Body>
-              <Card.Text>
-                <ul>
-                  <li>HTML</li>
-                  <li>CSS
+        <Row>
+          <Col sm={6} style={{
+          }}>
+            {[
+              'Dark',
+            ].map((variant) => (
+              <Card
+                bg={variant.toLowerCase()}
+                key={variant}
+                text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
+                style={{ width: '22rem', margin: '20px' }}
+                className="mb-2"
+              >
+                <Card.Header>Frontend</Card.Header>
+                <Card.Body>
+                  <Card.Text>
                     <ul>
-                      <li>Bootstrap</li>
+                      <li>HTML</li>
+                      <li>CSS
+                        <ul>
+                          <li>Bootstrap</li>
+                        </ul>
+                      </li>
+                      <li>JavaScript
+                        <ul>
+                          <li>Fundamental</li>
+                        </ul>
+                      </li>
+                      <li>React JS
+                        <ul>
+                          <li>React Bootstrap</li>
+                          <li>React Router</li>
+                          <li>State</li>
+                          <li>Props</li>
+                          <li>Lifecycle</li>
+                          <li>Fetch API uses Axios and React Query</li>
+                          <li>Payment Integration uses Midtrans</li>
+                        </ul>
+                      </li>
                     </ul>
-                  </li>
-                  <li>JavaScript
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            ))}
+          </Col>
+          <Col sm={5}>
+            {[
+              'Dark',
+            ].map((variant) => (
+              <Card
+                bg={variant.toLowerCase()}
+                key={variant}
+                text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
+                style={{ width: '22rem', margin: '20px' }}
+                className="mb-2"
+              >
+                <Card.Header>Backend</Card.Header>
+                <Card.Body>
+                  <Card.Text>
                     <ul>
-                      <li>Fundamental</li>
+                      <li>Go
+                        <ul>
+                          <li>Fundamental</li>
+                          <li>Routing uses gorilla/mux</li>
+                          <li>Database Connection uses PostgreSQL & MySQL</li>
+                          <li>Database uses GORM</li>
+                          <li>Encryption uses bycrypt</li>
+                          <li>Authentication uses JWT</li>
+                          <li>Payment Integration uses Midtrans</li>
+                          <li>Assets Managament uses Cloudinary</li>
+                        </ul>
+                      </li>
+                      <li>PHP
+                        <ul>
+                          <li>Fundamental</li>
+                          <li>CodeIgniter 3</li>
+                        </ul>
+                      </li>
+                      <li>Database
+                        <ul>
+                          <li>MySQL</li>
+                          <li>PostgreSQL</li>
+                          <li>DDL</li>
+                          <li>DML</li>
+                          <li>Table Relation</li>
+                        </ul>
+                      </li>
                     </ul>
-                  </li>
-                  <li>React JS
-                    <ul>
-                      <li>React Bootstrap</li>
-                      <li>React Router</li>
-                      <li>State</li>
-                      <li>Props</li>
-                      <li>Lifecycle</li>
-                      <li>Fetch API uses Axios and React Query</li>
-                      <li>Payment Integration uses Midtrans</li>
-                    </ul>
-                  </li>
-                </ul>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        ))}
-        {[
-          'Dark',
-        ].map((variant) => (
-          <Card
-            bg={variant.toLowerCase()}
-            key={variant}
-            text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
-            style={{ width: '25rem', margin: '20px' }}
-            className="mb-2"
-          >
-            <Card.Header>Backend</Card.Header>
-            <Card.Body>
-              <Card.Text>
-                <ul>
-                  <li>Go
-                    <ul>
-                      <li>Fundamental</li>
-                      <li>Routing uses gorilla/mux</li>
-                      <li>Database Connection uses PostgreSQL & MySQL</li>
-                      <li>Database uses GORM</li>
-                      <li>Encryption uses bycrypt</li>
-                      <li>Authentication uses JWT</li>
-                      <li>Payment Integration uses Midtrans</li>
-                      <li>Assets Managament uses Cloudinary</li>
-                    </ul>
-                  </li>
-                  <li>PHP
-                    <ul>
-                      <li>Fundamental</li>
-                      <li>CodeIgniter 3</li>
-                    </ul>
-                  </li>
-                  <li>Database
-                    <ul>
-                      <li>MySQL</li>
-                      <li>PostgreSQL</li>
-                      <li>DDL</li>
-                      <li>DML</li>
-                      <li>Table Relation</li>
-                    </ul>
-                  </li>
-                </ul>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        ))}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            ))}
+          </Col>
+        </Row>
       </div>
     </>
   );
